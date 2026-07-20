7342e7093ea7b5a26f550f66beb0a4cab2d29789
un <un@DESKTOP-JAU235M>
2026-07-20 21:25:56 +0300

    TODO: place Factorial result

---
+++ JavaScript/forms_and_events.js
@@ -0,0 +1,11 @@
+// JavaScript source code
+function Factorial() {
+	let numberElement = document.getElementById("number");
+	let number = numberElement.value;
+	let resultElement = document.getElementById("factorial-result");
+	let f = BigInt(1);
+	for (let i = 1n; i <= number; i++) {
+		f *= i;
+	}
+	resultElement.innerHTML = `${number}! = ${f}`;
+}
