cbed8bf47ed7c53339493e3f2cf7ddec1d5de2a0
un <un@DESKTOP-JAU235M>
2026-07-24 20:34:29 +0300

    switch background DONE, need optimisation

--- JavaScript/forms_and_events.js
+++ JavaScript/forms_and_events.js
@@ -11,12 +11,10 @@ function Factorial() {
 	resultElement.innerHTML = `${number}! = ${f}`;
 }
 
-function setImage()
-{
+function setImage() {
 	let filename = document.getElementById("image-file");
 	let reader = new FileReader();
-	reader.onload = function (e)
-	{
+	reader.onload = function (e) {
 		document.getElementById("image").src = e.target.result;
 	}
 	reader.readAsDataURL(filename.files[0]);
@@ -31,12 +29,27 @@ function setImage()
 //{
 //	document.body.style.color = document.getElementById("foreground-color").value;
 //}
-function setColor(event)
-{
+function setColor(event) {
 	//if (event.target.id === 'background-color') {
 	//	document.body.style.backgroundColor = event.target.value;
 	//}
 	//else
 	//	document.body.style.color = event.target.value;		
 	document.body.style[(event.target.id === 'background-color' ? 'background-color' : 'color')] = event.target.value
+}
+document.addEventListener("mousemove", traceMouse);
+function traceMouse(e) {
+	document.getElementById("mouse").innerHTML = `X= ${e.clientX}, Y = ${e.clientY}`;
+}
+
+document.getElementById("switch-background").addEventListener("click", switchBackground)
+function switchBackground(e) {
+
+	let skin = document.body.className;
+
+	let switchButton = document.getElementById("switch-background");
+	switchButton.src = skin === "dark" ? 'moon.png': 'sun.png';
+	document.body.className = skin === "dark" ? 'light': 'dark';
+	//document.getElementById("debug-background").innerHTML = switchButton.src;
+	document.getElementById("debug-background").innerHTML = document.body.className;
 }
